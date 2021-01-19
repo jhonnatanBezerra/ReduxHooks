12eb97ea88216cf8d456e26c779b5797d5b69be4
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2021-01-19 13:08:33 -0400

    ajustes cs

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './Store';
-import {Login} from './Pages/Login';
+import Routes from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Login />
+    <Routes />
   </Provider>,
   
   document.getElementById('root')
